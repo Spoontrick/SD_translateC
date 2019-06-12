12cc3cebfc415acb448ea8fe66b771afe2903c62
Spoontrick <51352674+Spoontrick@users.noreply.github.com>
2019-06-12 10:38:07 +0100

    Add files via upload

--- helpers.cpp
+++ helpers.cpp
@@ -1,9 +1,7 @@
 /**
 	helpers.cpp
-
 	Purpose: helper functions which are useful when
 	implementing a 2-dimensional histogram filter.
-
 	This file is incomplete! Your job is to make the
 	normalize and blur functions work. Feel free to 
 	look at helper.py for working implementations 
@@ -22,29 +20,23 @@ using namespace std;
 
 /**
 	TODO - implement this function
-
     Normalizes a grid of numbers. 
-
     @param grid - a two dimensional grid (vector of vectors of floats)
 		   where each entry represents the unnormalized probability 
 		   associated with that grid cell.
-
     @return - a new normalized two dimensional grid where the sum of 
     	   all probabilities is equal to one.
 */
-	
-
-vector < vector <float> > normalize(vector < vector <float> > grid);
-	
+		
 //function to normalize a 2D vector of floats
 vector < vector <float> > normalize(vector < vector <float> > grid) {
 
 	// declare a 2D vector with same size as grid
 	vector < vector <float> > newGrid (grid.size(), vector <float> (grid[0].size(), 0.0));
-
+float total = 0.0;
 for (int row = 0; row < grid.size(); row++) {
         for (int column = 0; column < grid[0].size(); column++) {
-        	float total = total + grid[row][column];
+        	total += grid[row][column];
         }
 }
     for (int row = 0; row < grid.size(); row++) {
@@ -60,34 +52,27 @@ for (int row = 0; row < grid.size(); row++) {
 
 /**
 	TODO - implement this function.
-
     Blurs (and normalizes) a grid of probabilities by spreading 
     probability from each cell over a 3x3 "window" of cells. This 
     function assumes a cyclic world where probability "spills 
     over" from the right edge to the left and bottom to top. 
-
     EXAMPLE - After blurring (with blurring=0.12) a localized 
     distribution like this:
-
     0.00  0.00  0.00 
     0.00  1.00  0.00
     0.00  0.00  0.00 
-
     would look like this:
 	
 	0.01  0.02	0.01
 	0.02  0.88	0.02
 	0.01  0.02  0.01
-
     @param grid - a two dimensional grid (vector of vectors of floats)
 		   where each entry represents the unnormalized probability 
 		   associated with that grid cell.
-
 	@param blurring - a floating point number between 0.0 and 1.0 
 		   which represents how much probability from one cell 
 		   "spills over" to it's neighbors. If it's 0.0, then no
 		   blurring occurs. 
-
     @return - a new normalized two dimensional grid where probability 
     	   has been blurred.
 */
@@ -95,7 +80,37 @@ vector < vector <float> > blur(vector < vector < float> > grid, float blurring) 
 
 	vector < vector <float> > newGrid;
 	
-	// your code here
+	int height = grid.size();
+	int width = grid[0].size();
+	float center = 1-blurring;
+	float side = blurring/6.0;
+	float corner = blurring/12.0;
+
+	vector< vector<float> > window = {
+	{corner, side, corner},
+	{side, center, side},
+	{corner, side, corner}
+	};
+
+
+	for (int i = 0; i < height; i++){
+		for (int j = 0; j< width; j++){
+			float grid_val = grid[i][j];
+			for (int dx = -1; dx<1; dx++){
+				for (int dy = -1; dy<1; dy++){
+					float mult = window[dx+1][dy+1];
+					int new_i = int (abs(i + dy)) % height;
+					int new_j = int (abs(j + dx)) % width;
+					newGrid[new_i][new_j] += mult * grid_val;
+			}
+
+			}
+
+
+		}
+
+
+	}
 
 	return normalize(newGrid);
 }
@@ -113,11 +128,9 @@ vector < vector <float> > blur(vector < vector < float> > grid, float blurring) 
     Determines when two grids of floating point numbers 
     are "close enough" that they should be considered 
     equal. Useful for battling "floating point errors".
-
     @param g1 - a grid of floats
     
     @param g2 - a grid of floats
-
     @return - A boolean (True or False) indicating whether
     these grids are (True) or are not (False) equal.
 */
@@ -152,9 +165,7 @@ bool close_enough(float v1, float v2) {
 
 /**
     Helper function for reading in map data
-
     @param s - a string representing one line of map data.
-
     @return - A row of chars, each of which represents the
     color of a cell in a grid world.
 */
@@ -179,9 +190,7 @@ vector <char> read_line(string s) {
 
 /**
     Helper function for reading in map data
-
     @param file_name - The filename where the map is stored.
-
     @return - A grid of chars representing a map.
 */
 vector < vector <char> > read_map(string file_name) {
@@ -204,18 +213,12 @@ vector < vector <char> > read_map(string file_name) {
 
 /**
     Creates a grid of zeros
-
     For example:
-
     zeros(2, 3) would return
-
     0.0  0.0  0.0
     0.0  0.0  0.0
-
     @param height - the height of the desired grid
-
     @param width - the width of the desired grid.
-
     @return a grid of zeros (floats)
 */
 vector < vector <float> > zeros(int height, int width) {
